8df899e8a9e6d5ab5ea7b9c8906b51dd0279f4e2
muba23 <muba2307@gmail.com>
2021-10-19 08:54:05 +0600

    firebase initialized

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD6qrXgKKWoN2Fez7YgNe7pIK4CMyHW0Jw",
+    authDomain: "we-care-hospital-f17f0.firebaseapp.com",
+    projectId: "we-care-hospital-f17f0",
+    storageBucket: "we-care-hospital-f17f0.appspot.com",
+    messagingSenderId: "230461400860",
+    appId: "1:230461400860:web:76a99a720013f9bc59fcd9"
+  };
+
+  export default firebaseConfig;
